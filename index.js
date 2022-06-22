2f4b5b04df0f9718247f0e7df5f4dfd616f0ff69
Utkp5 <utkp200@gmail.com>
2022-06-22 12:50:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log("hello world!");
+
+// let arr = [1,2,3,4,5]
+
+// first_method
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+
+// second method
+// arr.forEach((element) => {
+//     console.log(element);
+//   })
+
+// date method
+
+// console.log(new Date())
+// console.log(new Date().getTime());
+let s = "Utkarsh";
+const x = 12.345;
+console.log(s.substring(0,3));
+console.log(s.lastIndexOf("r"));
+console.log(s.slice(0,4));
+console.log(s.slice(-2));
+console.log(s.substr(1,4));
+console.log(s.toLocaleLowerCase());
+console.log(s.toLocaleUpperCase());
+console.log(s.concat(" ","pandit"));
+console.log(s.toLocaleLowerCase());
+console.log(x.toFixed(1));
