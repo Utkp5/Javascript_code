d1487c77afa252e5b0d7656a06f84621ad04a681
Utkp5 <utkp200@gmail.com>
2022-06-23 15:11:16 +0530

    modify

--- index.js
+++ index.js
@@ -17,15 +17,42 @@
 
 // console.log(new Date())
 // console.log(new Date().getTime());
-let s = "Utkarsh";
-const x = 12.345;
-console.log(s.substring(0,3));
-console.log(s.lastIndexOf("r"));
-console.log(s.slice(0,4));
-console.log(s.slice(-2));
-console.log(s.substr(1,4));
-console.log(s.toLocaleLowerCase());
-console.log(s.toLocaleUpperCase());
-console.log(s.concat(" ","pandit"));
-console.log(s.toLocaleLowerCase());
-console.log(x.toFixed(1));
+// let s = "Utkarsh";
+// const x = 12.345;
+// console.log(s.substring(0,3));
+// console.log(s.lastIndexOf("r"));
+// console.log(s.slice(0,4));
+// console.log(s.slice(-2));
+// console.log(s.substr(1,4));
+// console.log(s.toLocaleLowerCase());
+// console.log(s.toLocaleUpperCase());
+// console.log(s.concat(" ","pandit"));
+// console.log(s.toLocaleLowerCase());
+// console.log(x.toFixed(1));
+console.log(typeof "1");
+console.log(typeof true);
+
+// Object
+const obj = {
+    name : "utkp",
+    college : "IPU"
+};
+// console.log(obj);
+// console.log(obj.name);
+
+// coercion
+// if (1 == "1") {
+//     console.log(true);
+// }
+
+// traditional method 
+// function apple() {
+//     console.log("mango");
+// }
+// apple();
+
+const apple = () => {
+    console.log("banana");
+};
+
+apple();
