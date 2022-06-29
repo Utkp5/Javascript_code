29842e432dc7c88e5bae0cd57c5ea005662f1d31
Utkp5 <utkp200@gmail.com>
2022-06-29 11:52:29 +0530

    add notes

--- index.js
+++ index.js
@@ -29,14 +29,14 @@
 // console.log(s.concat(" ","pandit"));
 // console.log(s.toLocaleLowerCase());
 // console.log(x.toFixed(1));
-console.log(typeof "1");
-console.log(typeof true);
+// console.log(typeof "1");
+// console.log(typeof true);
 
 // Object
-const obj = {
-    name : "utkp",
-    college : "IPU"
-};
+// const obj = {
+//     name : "utkp",
+//     college : "IPU"
+// };
 // console.log(obj);
 // console.log(obj.name);
 
@@ -57,3 +57,216 @@ const obj = {
 // };
 
 // apple();
+
+
+
+// ****NOTES****
+
+//first
+// const s = "apple";
+
+// s = "car";
+
+// console.log(s.substring(0,3));
+
+//second
+
+// console.log(s.lastIndexOf("p"));
+
+//third
+// console.log(s.slice(0, 3));
+
+// console.log(s.slice(-3));
+
+// console.log(s.substring(-3));
+
+//fourth
+//start //length
+// console.log(s.substr(2, 2));
+
+//fifth
+
+// console.log(s.replace(s, "car"));
+
+//sixth
+// console.log(s.toLowerCase());
+
+// console.log(s.toUpperCase());
+
+//seven
+
+// console.log(s.concat(" ","mango"," ","mango"," ","mango","mango","mango"));
+
+//eigth
+
+// console.log(s);
+// console.log(s.trim());
+
+// const x = 12.355;
+
+// console.log(x.toFixed(1));
+
+// precedence --> () / "/"  / * / + / -
+
+//Data types
+
+//1. Primitive
+// Number, String, undefined, null, BigInt, Symbol, Boolean
+
+//2. Non-primitive
+// functions, arrays
+
+// console.log(typeof true);
+
+// const object = {
+//   //key         // value
+//   name    :       false,
+//   college :         1,
+//   var     :      "random",
+// };
+
+// console.log(object.name);
+
+//plus sign alwys prefers string addition operation
+
+// let s = 10 + 10;
+
+//minus operation always prefers calculation
+// let s = "utkarsh" - 10;
+// console.log(s);  output --> NaN
+
+//it was expecting a number but did not succeed
+// console.log(typeof NaN); // Not a number
+
+//coercion
+
+//checks data type as well as value
+// if (1 == "1") {
+//   console.log(true);
+// }
+
+//checks data type as well as value
+// if (1 === "1") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//traditional
+// function apple(parameter) {
+//   console.log(parameter);
+// }
+
+//arrow
+// const apple = (params) => {
+//   console.log(params);
+// };
+
+// apple("apple");
+
+// (function apple(parameter) {
+//   console.log(parameter);
+// })("Apple");
+
+// Functions
+// Functions are the vip's in js
+
+// Function can be assignd to a variable
+// function can be returned from the function
+// function can be passed to another function - Callback
+
+// 1st
+// var variableName = function (param) {
+//   console.log(param);
+// };
+
+// variableName();
+
+// variableName("apple");
+
+// 2nd
+
+// const variableName = function () {
+//   return function () {
+//     console.log("apple");
+//   };
+// };
+
+// variableName();
+
+// callbacks are the functions that are passed as an argument to another function
+
+// let, const, var
+
+// const name = "apple"; // constant
+
+// var name = "apple"; // for global scope, reassign , redeclare
+// var name;
+
+// console.log(name);
+
+// let
+
+// let name = "apple"; // reassign , usually within function
+
+// name = "mango";
+
+// console.log(name);
+
+// camel casing, underscore
+
+// 1'st camel casing
+// function returnsName(){
+
+// }
+
+// 2'nd underscore
+// function returns_name(){
+
+// }
+
+// why interpreted
+
+// console.log(x);
+
+// const x = 10;
+
+// why compiled
+
+//Hoisting
+
+// ftn();
+
+// const ftn = function () {
+//   console.log("apple");
+// };
+
+// currying
+// function ftn(a) { // first second third
+//   return (b) => {
+//     return (c) => {
+//       return a + b;
+//     };
+//   };
+// }
+
+//function
+// function ftn2(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log(ftn2(10, 20, 30));
+
+// console.log(ftn(10)(20)); 
+
+// Hold
+// function apple() {
+//   return function calc() {
+//     return 10;
+//   };
+// }
+
+// var test = apple();
+
+// console.log(apple());
+
