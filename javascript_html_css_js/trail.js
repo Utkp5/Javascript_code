3773984137ffb5e525e4eab2e19b3415b8face56
Utkp5 <utkp200@gmail.com>
2022-06-29 13:20:46 +0530

    new folder

---
+++ javascript_html_css_js/trail.js
@@ -0,0 +1,9 @@
+function color1() {
+    document.getElementById("hello1").style.backgroundColor = "orange"  
+}
+function color2() {
+    document.getElementById("hello2").style.backgroundColor = "lightblue"
+}
+function color3() {
+    document.getElementById("hello3").style.backgroundColor = "green"
+} 
