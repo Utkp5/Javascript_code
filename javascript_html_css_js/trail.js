281f1b2cd6fb8b75a8b73e347ad2cd86346f5204
Utkp5 <utkp200@gmail.com>
2022-06-30 13:26:11 +0530

    update

--- javascript_html_css_js/trail.js
+++ javascript_html_css_js/trail.js
@@ -7,3 +7,14 @@ function color2() {
 function color3() {
     document.getElementById("hello3").style.backgroundColor = "green"
 } 
+function colorchng()
+{
+
+    let colorchange = document.getElementsByClassName("goodMorning")
+    for (let i = 0; i < colorchange.length; i++) 
+    {
+        colorchange[i].style.backgroundColor = "crimson";
+        colorchange[i].innerHTML = " Welcome Utkarsh"
+
+    }
+}
