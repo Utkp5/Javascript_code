bc7d719d537dc6b1a2cff06f347baf9d8f4b940f
PanditUtkarsh <panditutkarsh555@gmail.com>
2022-08-04 01:58:33 +0530

    New Files

---
+++ string.js
@@ -0,0 +1,10 @@
+//Template Literals
+
+let name1 = "rahat";
+let name2 = "pandit";
+
+let result = `name1 with name2`;
+let result1 = `${name1} with ${name2}`   //<-- String Interpolation
+
+console.log(result);
+console.log(result1);
