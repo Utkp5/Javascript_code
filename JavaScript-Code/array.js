5c67a3801364a5eaf8a530597e872e32281296a1
PanditUtkarsh <panditutkarsh555@gmail.com>
2022-08-07 00:56:27 +0530

    New Files

---
+++ JavaScript-Code/array.js
@@ -0,0 +1,18 @@
+// Array is a variable that is used to store more than one value at contigious location 
+// in js Array are object
+
+let marks = [1,2,3,4,5];
+
+console.log(marks); // it will return asetive same to same on console
+
+console.log(marks[2]); // it will return the actual value that is stored in an array
+
+
+// another method to access Array
+let fruits = ["mango","apple","banana","pineapple","stawberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    
+    console.log(`Fruit No ${i} ` + fruits[i]);
+}
+
