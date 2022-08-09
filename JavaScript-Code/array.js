bcd4e3048bc8cc2b8187bc63efe19c66c4e60401
PanditUtkarsh <panditutkarsh555@gmail.com>
2022-08-10 01:30:01 +0530

    New Files

--- JavaScript-Code/array.js
+++ JavaScript-Code/array.js
@@ -1,18 +1,35 @@
 // Array is a variable that is used to store more than one value at contigious location 
 // in js Array are object
 
-let marks = [1,2,3,4,5];
+// let marks = [1,2,3,4,5];
 
-console.log(marks); // it will return asetive same to same on console
+// console.log(marks); // it will return asetive same to same on console
 
-console.log(marks[2]); // it will return the actual value that is stored in an array
+// console.log(marks[2]); // it will return the actual value that is stored in an array
 
 
 // another method to access Array
-let fruits = ["mango","apple","banana","pineapple","stawberry"];
+// let fruits = ["mango","apple","banana","pineapple","stawberry"];
 
-for (let i = 0; i < fruits.length; i++) {
+// for (let i = 0; i < fruits.length; i++) {
     
-    console.log(`Fruit No ${i} ` + fruits[i]);
-}
+//     console.log(`Fruit No ${i} ` + fruits[i]);
+// }
+
+
+// --------------------------
+
+let num = [1,2,3,4,5,6]
+let b = num.toString(); //num ko string me convert kr diya 
+console.log(b);
+
+let c = num.join(" and ")
+console.log(c);
+
+
+// pop (pop the elements from end)|| push || shift (shift the elements from start) || unshift  
+//shift and pop are one and the same thing they both are doing same functionality
+
+let x = num.push(7);
+console.log(num);
 
