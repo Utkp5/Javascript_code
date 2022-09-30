945ae0e0419dcfe5aa7bb1ad12edb8ce0ecbb7a0
Utkp5 <utkp200@gmail.com>
2022-09-30 21:14:58 +0530

    setinterval

---
+++ JavaScript-Code/setinterval.js
@@ -0,0 +1,11 @@
+console.log("Start");
+
+// setTimeout( function(){ // it will run after this time
+
+//     console.log("hello utkarsh");
+
+// },5000)
+
+setInterval( function () { // it will execute continously until we stop
+    console.log("utkarsh");
+},2000)
