acb9e808fdd612a25bca107a5ed8d6597be2effc
Utkp5 <utkp200@gmail.com>
2022-10-09 13:01:26 +0530

    async

---
+++ JavaScript-Code/async_sync.js
@@ -0,0 +1,12 @@
+//example of sync in this code will run line wise.
+// console.log("hello");
+// console.log("gm");
+// console.log("ok");
+
+
+//eg of async  in this the code run || 
+console.log("starting");
+setTimeout(() => {
+    console.log("work done");
+}, 2000);
+console.log("end");
